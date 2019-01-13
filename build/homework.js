872bd93d5533bee9b61c0c955bda0a959df8fe82
coolinich <coolinich@gmail.com>
2019-01-13 22:06:43 +0200

    Added Car classes

--- build/homework.js
+++ build/homework.js
@@ -1,78 +1,56 @@
 // Car - abstract class which describe car
-abstract class Car {
-    protected mileage: number;
-    protected fuel: number;
-    
-    public abstract drive(distance: number): void;
-    public abstract refuel(volume: number): void;
+class Car {
 }
-
 // RealCar - class which inherits from abstract class Car
 class RealCar extends Car {
-    tankCapacity: number;
-    fuelConsumption: number;
-
-    constructor(
-        tankCapacity: number, 
-        mileage:number, 
-        fuel:number, 
-        fuelConsumption: number
-        ) {
-        super(); 
+    constructor(tankCapacity, mileage, fuel, fuelConsumption) {
+        super();
         this.mileage = mileage;
         this.fuel = fuel;
         this.tankCapacity = tankCapacity;
         this.fuelConsumption = fuelConsumption;
-    };
-
-    /** 
+    }
+    ;
+    /**
      * fuelTank - getter for fuel property
     */
-    public get fuelTank(): number {
+    get fuelTank() {
         return this.fuel;
     }
-
-    
-    /** 
+    /**
      * mileageTotal - getter for mileage property
     */
-    public get mileageTotal(): number {
+    get mileageTotal() {
         return this.mileage;
     }
-
     /**
      * drive - function which updates data according to received distance value
      * @param {number} distance length of trip
      */
-    drive(distance: number): void {
-        let requiredFuel = ( distance / 100 ) * this.fuelConsumption;
+    drive(distance) {
+        let requiredFuel = (distance / 100) * this.fuelConsumption;
         if (this.fuel > requiredFuel) {
             this.mileage += distance;
             this.fuel = this.fuel - requiredFuel;
-        } else { 
+        }
+        else {
             this.mileage += this.fuel / this.fuelConsumption * 100;
             this.fuel = 0;
             console.log("Your tank is empty, sorry!");
-        } 
+        }
     }
-
     /** refuel - function which updates fuel value
      * @param {number} volume increment of fuel value
      */
-    refuel(volume: number): void {
+    refuel(volume) {
         this.fuel += volume;
-        this.fuel = this.fuel > this.tankCapacity ? this.tankCapacity : this.fuel; 
+        this.fuel = this.fuel > this.tankCapacity ? this.tankCapacity : this.fuel;
     }
 }
-
-
 const newCar = new RealCar(30, 120, 0, 5);
 newCar.refuel(5);
-console.log( newCar.fuelTank );
-console.log( newCar.mileageTotal );
+console.log(newCar.fuelTank);
+console.log(newCar.mileageTotal);
 newCar.drive(600);
-console.log( newCar.fuelTank );
-console.log( newCar.mileageTotal );
-
-
-
+console.log(newCar.fuelTank);
+console.log(newCar.mileageTotal);
